2c8ed9816ca639003c758bbe53d92f73198d1fb2
Alexandr Kabanau <Azraill1989@gmail.com>
2023-10-04 23:20:49 +0300

    feat: 12 install redux-toolkit

--- react_pizza/src/App.js
+++ react_pizza/src/App.js
@@ -12,6 +12,9 @@ import { Cart } from './pages/Cart';
 import { NotFound } from './pages/NotFound';
 import { Route, Routes } from 'react-router-dom';
 import { createContext, useState } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { increment, decrement } from './redux/slices/filterSlice';
+
 
 // import pizzas from "./assets/pizzas.json"
 
@@ -19,20 +22,36 @@ export const SearchContext = createContext();
 
 function App() {
 
-  const [searchValue, setSearchValue] = useState('')
+  // const [searchValue, setSearchValue] = useState('');
+  const count = useSelector((state) => state.counter.value)
+  const dispatch = useDispatch()
 
   return (
     <div className="wrapper">
-      <SearchContext.Provider value={{ searchValue, setSearchValue }}>
-        <Header />
-        <div className="content">
-          <Routes>
-            <Route path='/' element={<Home />} />
-            <Route path='/cart' element={<Cart />} />
-            <Route path='*' element={<NotFound />} />
-          </Routes>
-        </div>
-      </SearchContext.Provider>
+
+      <button
+        aria-label="Increment value"
+        onClick={() => dispatch(increment())}
+      >
+        Increment
+      </button>
+      <span>{count}</span>
+      <button
+        aria-label="Decrement value"
+        onClick={() => dispatch(decrement())}
+      >
+        Decrement
+      </button>
+      {/* <SearchContext.Provider value={{ searchValue, setSearchValue }}> */}
+      {/* <Header /> */}
+      <div className="content">
+        {/* <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/cart' element={<Cart />} />
+          <Route path='*' element={<NotFound />} />
+        </Routes> */}
+      </div>
+      {/* </SearchContext.Provider> */}
     </div>
   );
 }
