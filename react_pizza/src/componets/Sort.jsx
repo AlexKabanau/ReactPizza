ffb125b828a15c4ec7ec38aac5cdd43dbc690a76
AlexKabanau <azraill1989@gmail.com>
2023-10-10 15:57:04 +0300

    feat: 16 addCart sum part1

--- react_pizza/src/componets/Sort.jsx
+++ react_pizza/src/componets/Sort.jsx
@@ -1,4 +1,5 @@
-import React, { useState } from 'react';
+import React, { useRef, useState } from 'react';
+import { useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { setSort } from '../redux/slices/filterSlice';
 
@@ -15,20 +16,32 @@ function Sort() {
 
   const dispatch = useDispatch();
   const sort = useSelector(state => state.filter.sort)
+  const sortRef = useRef();
 
   const [open, setOpen] = useState(false);
 
-  // const sortName = list[sort].name;
-
   const onClickListItem = (obj) => {
-    // onChangeSort(index);
     dispatch(setSort(obj))
     setOpen(false)
 
   }
 
+
+  useEffect(() => {
+    const handleClickOutside = (event) => {
+      if (!event.composedPath().includes(sortRef.current)) {
+        setOpen(false)
+      }
+    }
+    document.body.addEventListener('click', handleClickOutside)
+
+    return () => {
+      document.body.removeEventListener('click', handleClickOutside)
+    }
+  }, [])
+
   return (
-    <div className="sort">
+    <div ref={sortRef} className="sort">
       <div className="sort__label">
         <svg
           width="10"
