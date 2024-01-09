df828e5078440c9e9ff486e6f1234a9a490fa6f8
Alexandr Kabanau <Azraill1989@gmail.com>
2024-01-09 22:34:07 +0300

    feat: 19 useParams, useLocation, Outlet after merg

--- react_pizza/src/App.js
+++ react_pizza/src/App.js
@@ -15,21 +15,10 @@ import { Route, Routes } from 'react-router-dom';
 import FullPizza from './pages/FullPizza';
 import MainLayout from './layouts/MainLayout';
 
-<<<<<<< HEAD
-=======
-// import pizzas from "./assets/pizzas.json"
-import FullPizza from './pages/FullPizza';
-
-// export const SearchContext = createContext();
->>>>>>> 4940ae4e41e38fc9b705b486e9af152bf343fda5
 
 function App() {
 
-
-
-
   return (
-<<<<<<< HEAD
     <Routes>
       <Route path='/' element={<MainLayout />}>
         <Route path='/' element={<Home />} />
@@ -39,21 +28,6 @@ function App() {
       </Route>
 
     </Routes>
-=======
-    <div className="wrapper">
-      {/* <SearchContext.Provider value={{ searchValue, setSearchValue }}> */}
-      <Header />
-      <div className="content">
-        <Routes>
-          <Route path='/' element={<Home />} />
-          <Route path='/cart' element={<Cart />} />
-          <Route path='/pizza/:id' element={<FullPizza />} />
-          <Route path='*' element={<NotFound />} />
-        </Routes>
-      </div>
-      {/* </SearchContext.Provider> */}
-    </div>
->>>>>>> 4940ae4e41e38fc9b705b486e9af152bf343fda5
   );
 }
 
